32d55942459e32cd6a50139546a1530cfc949027
Alexandr Vigriyanov <lexnnov2009@gmail.com>
2020-06-28 23:13:35 +0300

    IE

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import 'core-js/es/map';
+import 'core-js/es/set';
 import React from 'react';
 import './App.scss';
 import { Route, Switch } from 'react-router-dom'
@@ -6,15 +8,15 @@ import Users from './Pages/Users/Users';
 import Photos from './Pages/Photos/Photos';
 
 function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" component={Users} />
-        <Route exact path="/albums/:id" component={Albums} />
-        <Route exact path={`/albums/:id/photo/:id`} component={Photos} />
-      </Switch>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Switch>
+				<Route exact path="/" component={ Users }/>
+				<Route exact path="/albums/:id" component={ Albums }/>
+				<Route exact path={ `/albums/:id/photo/:id` } component={ Photos }/>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
